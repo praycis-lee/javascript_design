22bf4dc390a739c8a7ce08d8719d0363ab513efb
leexiaoyong <xiaoyong.l@moneplus.cn>
2017-07-26 00:15:07 +0800

    update

--- 设计模式demo/原型式继承.js
+++ 设计模式demo/原型式继承.js
@@ -7,10 +7,13 @@ var Person = {
 }
 
 
-function clone(object){
+function clone(object, name){
 	var F = function(){};
 	F.prototype = object;
+	object.name = name;
 	return new F();
 }
 
-var a = clone(Person);
+var a = clone(Person, 'zhangsan');
+
+console.log(a.getName());
